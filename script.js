83e9477015487862f55e9efd526f4d722fe51168
Aman kumar Singh <amankrsingh8969@gmail.com>
2025-01-16 11:54:58 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleAnswer(container) {
-    const answer = container.nextElementSibling;
-    const toggle = container.querySelector(".faq-toggle");
-    answer.classList.toggle("visible");
-    toggle.textContent = answer.classList.contains("visible") ? "-" : "+";
-  }
