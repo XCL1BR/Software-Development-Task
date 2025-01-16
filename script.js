d771941e90e57ece747bbc2e553d3d39e36ff871
Aman Kumar Singh <amankrsingh8969@gmail.com>
2025-01-16 11:20:58 +0530

    code

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleAnswer(container) {
+    const answer = container.nextElementSibling;
+    const toggle = container.querySelector(".faq-toggle");
+    answer.classList.toggle("visible");
+    toggle.textContent = answer.classList.contains("visible") ? "-" : "+";
+  }
+  
